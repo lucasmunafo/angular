5c818e75b8ea22df048dc1863399d292b618dddf
Lucas Munafo <lucasmunafo97@gmail.com>
2018-10-29 13:04:10 -0300

    If-else, for, Switch-case

---
+++ src/app/profesor/profesor.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Profesor } from './profesor';
+
+@Component({
+  selector: 'app-profesor',
+  templateUrl: './profesor.component.html',
+  styleUrls: ['./profesor.component.css']
+})
+export class ProfesorComponent implements OnInit {
+
+    public nombre:string;
+    public edad:number;
+    public casado:boolean;
+    public trabajos:Array<any> = ['Profesor', 'Webdev', 'Barrendero'];
+
+    //LLAMAMOS AL MODELO PROFESOR
+    public profesor: Profesor;
+    public profesorado: Array<Profesor>;
+    public color:string;
+
+
+  constructor(){
+    this.nombre= 'Lucas';
+    this.edad= 21;
+    this.casado=false;
+    this.trabajos=['Alumno','Webdev','bosques de palermo'];
+
+
+    this.profesor  = new Profesor('Lucas',21,'Veterinario',true);
+    this.profesorado = [
+      this.profesor,
+      new Profesor('Vanesa',41,'Historia',true),
+      new Profesor('Roberto',21,'Matematica',true)
+    ]
+
+    this.color='red';
+}
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/profesor/profesor.ts
@@ -0,0 +1,9 @@
+// MODELO
+export class Profesor {
+    constructor(
+        public nombre:string,
+        public edad:number,
+        public materia:string,
+        public contratado:boolean
+    ){}
+}
